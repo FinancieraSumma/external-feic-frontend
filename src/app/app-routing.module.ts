6d857c12b892087821e53a85b572e4b908b2bebd
Lino Garcia Vallejo <lino.garciavallejo@outlook.com>
2024-11-12 23:05:33 -0600

    Add new routes for login verification, password recovery, and main components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserRegistrationComponent } from './features/user-registration/user-registration.component';
 import { UserVerificationComponent } from './features/user-verification/user-verification.component';
 import { LoginComponent } from './features/login/login.component';
+import { LoginVerificationComponent } from './features/login-verification/login-verification.component';
+import { ForgotPasswordComponent } from './features/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './features/reset-password/reset-password.component';
+import { LogoutComponent } from './features/logout/logout.component';
+import { MainComponent } from './features/main/main.component';
 
 const routes: Routes = [
   { path: 'register', component: UserRegistrationComponent },
   { path: 'verify', component: UserVerificationComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'verifyLogin', component: LoginVerificationComponent },
+  { path: 'forgotPassword', component: ForgotPasswordComponent },
+  { path: 'resetPassword', component: ResetPasswordComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: 'main', component: MainComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
